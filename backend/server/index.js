90b3cc9ae575aa3aeb39dfac98caa23afd4ecc89
PabloD97 <pablo97758@gmail.com>
2021-08-09 09:10:18 -0300

    feat: Endpoints Receta
    - Se agregaron los endpoints correspondientes para comunicarse con RecetaController

--- backend/server/index.js
+++ backend/server/index.js
@@ -7,7 +7,7 @@ server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
 server.use(cors());
 
-
+// TODO: Crear una carpeta routers que se encargue de los endpoints
 server.get("/api/ingredientes", IngredienteController.obtenerIngredientes);
 server.get("/api/recetas", RecetaController.obtenerRecetas);
 
@@ -16,15 +16,30 @@ server.post(
   "/api/agregar_ingrediente",
   IngredienteController.agregarIngrediente
 );
+server.post(
+  "/api/agregar_receta",
+  RecetaController.agregarReceta
+);
+
 
 server.delete(
   "/api/eliminar_ingrediente/:id",
   IngredienteController.eliminarIngrediente
 );
+server.delete(
+  "/api/eliminar_receta/:id",
+  RecetaController.eliminarReceta
+);
+
 
 server.put(
   "/api/actualizar_ingrediente/:id",
   IngredienteController.actualizarIngrediente
 );
+server.put(
+  "/api/actualizar_receta/:id",
+  RecetaController.actualizarReceta
+);
+
 
 module.exports = server;
