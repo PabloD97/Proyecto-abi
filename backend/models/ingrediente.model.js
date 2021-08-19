50fa7e7b88eb7b78fd108055853ee6d35789dd61
PabloD97 <pablo97758@gmail.com>
2021-08-19 18:35:16 -0300

    feat: Ingrediente model
    - Se agrego el field esGramos al modelo Ingrediente

--- backend/models/ingrediente.model.js
+++ backend/models/ingrediente.model.js
@@ -5,6 +5,7 @@ const IngredienteSchema = new Schema(
   {
     nombre: { type: String, require: true },
     precioKg: { type: Number, require: true },
+    esGramos: { type: Boolean, require: true}
   },
   { timestamps: { createdAt: true, updatedAt: true } }
 );
